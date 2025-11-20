8021d7c7ad1fc2b632e92c6319eb9b1037bfb642
infonautic.org <242398526+infonauticDotOrg@users.noreply.github.com>
2025-11-20 16:19:40 +0100

    Add first iteration of getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("if you see me, js works!");
+function getComputerChoice(){
+    let cpuChoice = Math.floor(Math.random() * 3);
+    console.log(cpuChoice);
+    if (cpuChoice == 1 ){
+        return "rock"
+    }else if (cpuChoice == 2){
+        return "paper"
+    }else{
+        return "scissors"
+    }
+}

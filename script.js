cadfd9385ff3d222c3ef72fbc743866b718a84ef
infonautic.org <242398526+infonauticDotOrg@users.noreply.github.com>
2025-11-20 17:04:05 +0100

    Add initial version of playRound fn

--- script.js
+++ script.js
@@ -1,17 +1,57 @@
+let humanScore = 0;
+let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection)
 function getComputerChoice(){
-    let cpuChoice = Math.floor(Math.random() * 3);
-    console.log(cpuChoice);
-    if (cpuChoice == 1 ){
-        return "rock"
-    }else if (cpuChoice == 2){
-        return "paper"
+    let computerChoice = Math.floor(Math.random() * 3);
+    console.log(computerChoice);
+    if (computerChoice === 1 ){
+        return "paper";
+    }else if (computerChoice === 2){
+        return "scissors";
     }else{
-        return "scissors"
+        return "rock";
     }
 }
 
 function getHumanChoice(){
    let humanChoice =  prompt("CHOOSE YOUR FIGHTER: Rock, Paper, or Scissors? What will it be?")
-   console.log(humanChoice)
-   return humanChoice
+   console.log(humanChoice);
+   return humanChoice;
+}
+
+function playRound(human, computer){
+    humanLower = human.toLowerCase();
+    computerLower = computer.toLowerCase();
+    console.log(humanLower)
+    console.log(computerLower)
+    if (humanLower === "rock" && computerLower === "paper"){
+        alert("You lose!");
+        computerScore++;
+    }else if (humanLower === "rock" && computerLower === "scissors"){
+        alert("You win!");
+        humanScore++;
+    }else if (humanLower === "rock" && computerLower === "rock"){
+        alert("Tie!Play again");
+    }else if (humanLower === "paper" && computerLower === "rock"){
+        alert("You win!");
+        humanScore++;
+    }else if (humanLower === "paper" && computerLower === "scissors"){
+        alert("You lose!");
+        computerScore++;
+    }else if (humanLower === "paper" && computerLower === "paper"){
+        alert("Tie!Play again");
+    }else if (humanLower === "scissors" && computerLower === "rock"){
+        alert("You lose!");
+        computerScore++;
+    }else if (humanLower === "scissors" && computerLower === "paper"){
+        alert("You win!");
+        humanScore++;
+    }else if (humanLower === "scissors" && computerLower === "scissors"){
+        alert("Tie!Play again");
+    }
+    alert(humanSelection + computerSelection)
 }
